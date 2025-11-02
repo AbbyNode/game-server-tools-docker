79c06a6dfb92b442f846c8da97ba9d55fb9204e2
Lucas <AbbyNode@users.noreply.github.com>
2025-11-01 21:41:22 -0400

    entry

--- mcaselector/scripts/entrypoint.sh
+++ mcaselector/scripts/entrypoint.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# Create config options file
+if [ ! -f /config/mcaselector-options.yaml ]; then
+    cp /templates/mcaselector-options.yaml /config/mcaselector-options.yaml
+fi
+
+exec /mcaselector/delete-chunks.sh
